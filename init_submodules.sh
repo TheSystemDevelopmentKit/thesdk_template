a28de2805b383c483840572becc33034a5f06c2c
Marko Kosunen <marko.kosunen@aalto.fi>
2019-06-18 16:07:29 -0700

    Make init_submodule recursive

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #Selective initialization of submodules
 #Written by by Marko Kosunen, marko.kosunen@aalto.fi, 2017
+DIR=$( cd `dirname $0` && pwd )
 SUBMODULES="\
     ./Entities/verilog \
     ./Entities/vhdl \
@@ -13,5 +14,13 @@ SUBMODULES="\
 git submodule sync
 for mod in $SUBMODULES; do 
     git submodule update --init $mod
+    cd ${mod}
+    if [ -f ./init_submodules.sh ]; then
+        ./init_submodules
+    fi
+    cd ${DIR}
+
 done
+exit
+
 
