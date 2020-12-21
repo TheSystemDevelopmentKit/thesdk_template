36fd65765aac910a48ce5dc340a59fed6e861626
Marko Kosunen <marko.kosunen@aalto.fi>
2020-12-21 07:03:36 +0200

    Relocate pip3.6userinstall, README and thesdk_helpers
    -Thesdk helpers ins now in project root
    -README is linked from thesdk_helpers/common
    -pip3.6userinstall.sh is linked from thesdk_helpers/shell

--- pip3.6userinstall.sh
+++ pip3.6userinstall.sh
@@ -1,13 +1,1 @@
-#/usr/bin/env bash
-
-#Installs the missing python modules locally with pip3.6
-pip3.6 install --user numpy
-pip3.6 install --user matplotlib
-pip3.6 install --user joblib
-pip3.6 install --user scipy
-pip3.6 install --user pandas
-pip3.6 install --user sphinx
-pip3.6 install --user sphinx_rtd_theme
-
-exit 0
-
+thesdk_helpers/shell/pip3.6userinstall.sh
