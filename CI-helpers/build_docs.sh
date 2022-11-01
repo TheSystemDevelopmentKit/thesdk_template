0f7e2276e1acd3950a9a4baebf5e716c2eb89c28
Marko Kosunen <marko.kosunen@aalto.fi>
2022-11-01 16:13:52 +0200

    Update docbuild
    Prevent changing the user identity in non-CI builds

--- CI-helpers/build_docs.sh
+++ CI-helpers/build_docs.sh
@@ -1,14 +1,62 @@
 #!/bin/sh -l
 #############################################################################
 # This is a documentation builder script for TheSyDeKick
+
 # 
 # Written by Marko Kosunen, marko.kosunen@aalto.fi, 2022
 #############################################################################
 
+help_f()
+{
+cat << EOF    
+build_docs Release 1.0 (1.11.2022)
+For building documentationof  TheSyDeKick releases
+Written by Marko Pikkis Kosunen
+
+SYNOPSIS
+  build_docs.sh [OPTIONS]
+DESCRIPTION
+   Defines and runs tests for the submodules of thesdk_template
+
+OPTIONS
+  -b 
+     Branch of thesdk_template to operate on
+     Commit and push to that branch after testing.
+
+  -c Run in CI/CD with this option 
+
+  -t
+     STRING : Access token 
+  -h
+      Show this help.
+EOF
+}
+
 
 # Token we use for push is given as the first argument
-TOKEN=$1
+CICD="0"
+TOKEN=""
+BRANCH=""
+while getopts b:ct:h opt
+do
+  case "$opt" in
+    b) BRANCH="$OPTARG";;
+    c) CICD="1";;
+    t) TOKEN="$OPTARG";;
+    h) help_f; exit 0;;
+    \?) help_f;;
+  esac
+done
 
+#if [ -z "${BRANCH}" ]; then
+#    echo "Branch not given"
+#    exit 1
+#fi
+
+if [ -z "$TOKEN" ]; then
+    echo "Token must be provided for CI/CD"
+    exit 1
+fi
 # Assumption is that we are working in the cloe of this project.
 
 # Local pip-installations to follow the dependencies of the main program
@@ -47,8 +95,11 @@ git clone https://github.com/TheSystemDevelopmentKit/docs.git
 cd docs && git checkout main && git pull
 cp -rp ../build/html/* ./
 git add -A
-git config --global user.name "ecdbot"
-git config --global user.email "${GITHUB_ACTOR}@noreply.github.com"
+if [ "$CICD" == "1" ]; then
+    git config --global user.name "ecdbot"
+    git config --global user.email "${GITHUB_ACTOR}@noreply.github.com"
+fi
+
 git commit -m"Update docs" 
 
 # We ´push this to a different place than this project.
