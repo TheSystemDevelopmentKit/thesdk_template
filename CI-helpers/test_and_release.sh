3abf079cc72cba668cb56f4c314ffc767977a4e7
Marko Kosunen <marko.kosunen@aalto.fi>
2025-08-24 07:41:43 +0300

    Update and test CI flow

--- CI-helpers/test_and_release.sh
+++ CI-helpers/test_and_release.sh
@@ -142,9 +142,10 @@ fi
 # If we update, then we also test
 cd $TEMPLATEDIR
 # Let's perform the test(s)
-cd ${TEMPLATEDIR}/doc && make html
-DOCSTAT=$?
-DOCSTAT="0"
+# Docbuild handled elsewhere
+#cd ${TEMPLATEDIR}/doc && make html
+#DOCSTAT=$?
+#DOCSTAT="0"
 
 for entity in inverter myentity inverter_tests; do
     cd ${TEMPLATEDIR}/Entities/${entity} && ./configure &&  make sim
